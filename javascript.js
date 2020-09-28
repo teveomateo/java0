fa1fee96f207af662f081a3140a7016bd3857562
Mateo Barroetavena <maba1731@colorado.edu>
2020-09-27 23:52:16 -0600

    new file:   friends.jpg 	modified:   index.html 	modified:   javascript.js

--- javascript.js
+++ javascript.js
@@ -6,34 +6,106 @@ acostado.onclick = function(){
 document.getElementById("laFoto").src = 'lay.jpg';   
 };
 
+function mOver1(obj){
+obj.innerHTML = "Want me to lay?"};
+
+function mOut1(obj) {
+obj.innerHTML = "Lay"
+};
+
+
 banar.onclick = function(){
 document.getElementById("laFoto").src = 'bathe.jpg';   
+};
+
+function mOver2(obj){
+obj.innerHTML = "Want me to bathe?"};
+
+function mOut2(obj) {
+obj.innerHTML = "Bathe"
 };
 
 comer.onclick = function(){
 document.getElementById("laFoto").src = 'eat.jpg';   
 };
 
+function mOver3(obj){
+obj.innerHTML = "Want me to Eat?"};
+
+function mOut3(obj) {
+obj.innerHTML = "Eat"
+};
+
 parque.onclick = function(){
 document.getElementById("laFoto").src = 'park.jpg';   
+};
+
+function mOver4(obj){
+obj.innerHTML = "The park!?"};
+
+function mOut4(obj) {
+obj.innerHTML = "Park"
 };
 
 pata.onclick = function(){
 document.getElementById("laFoto").src = 'paw.jpg';   
 };
 
+function mOver5(obj){
+obj.innerHTML = "My paw?"};
+
+function mOut5(obj) {
+obj.innerHTML = "Paw"
+};
+
+
 cachorro.onclick = function(){
 document.getElementById("laFoto").src = 'puppy.jpg';   
+};
+
+
+function mOver6(obj){
+obj.innerHTML = "Me as a pup?"};
+
+function mOut6(obj) {
+obj.innerHTML = "Puppy"
 };
 
 sentado.onclick = function(){
 document.getElementById("laFoto").src = 'sit.jpg';   
 };
 
+function mOver7(obj){
+obj.innerHTML = "Want me to sit?"};
+
+function mOut7(obj) {
+obj.innerHTML = "Sit"
+};
+
 premio.onclick = function(){
 document.getElementById("laFoto").src = 'treat.jpg';   
 };
 
+function mOver8(obj){
+obj.innerHTML = "WHAT?!"};
+
+function mOut8(obj) {
+obj.innerHTML = "Treat"
+};
+
 busca.onclick = function(){
-    document.getElementById("laFoto").src = 'fetch.jpg';   
-    };
+document.getElementById("laFoto").src = 'fetch.jpg';   
+};
+
+function mOver9(obj){
+obj.innerHTML = "Are you gonna throw?"};
+
+function mOut9(obj) {
+obj.innerHTML = "Fetch"
+};
+
+function mOver10(obj){
+    obj.src= "friends.jpg"};
+    function mOut10(obj) {
+        obj.src= "labrador-home.jpg"
+        };
